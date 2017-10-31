f050e664c5941088d920620ee2329c2d4bcc0a75
lis55 <lis_lou77@msn.com>
2017-10-30 21:14:02 -0400

    Add files via upload

--- matrix.cpp
+++ matrix.cpp
@@ -1,29 +1,35 @@
 #include<iostream>
+using namespace std;
 
 class Matrix{
     int rows, columns;
     double** matrix;
     public:
     Matrix(int,int);
-    double setrow();
 
 };
 
 Matrix::Matrix(int x, int y){
     rows=y;
     columns=x;
-};
-
-double Matrix::setrow(){
     matrix=new double*[columns];
+    for(int i = 0; i < columns; ++i){
+            matrix[i]=new double[rows];
+    };
     for(int i=0;i<rows;i++){
-        matrix[i]=new double[rows];
+        for(int j=0;j<columns;j++){
+          matrix[i][j]=0;
     };
 };
+};
+
+
 
 int main(){
 
+Matrix AA(2,2);
 
+cout << AA[0][0];
 
 return 0;
 }
