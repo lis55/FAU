9f059b3ef8906fa6d8399adb40b3caba6d94a8df
lis55 <lis_lou77@msn.com>
2017-11-12 12:02:12 -0400

    Add files via upload

--- MatrixProduct.cpp
+++ MatrixProduct.cpp
@@ -1,6 +1,8 @@
+
 #include <iostream>
 #include <typeinfo>
 #include "Matrix.h"
+#include <assert.h>
 using namespace std;
 
 int main(){
@@ -11,7 +13,10 @@ cin>>s1;
 cin>>s2;
 cin>>s3;
 
-if (typeid(s1) != typeid(size_t()) || typeid(s2) != typeid(size_t()) || typeid(s3) != typeid(size_t()))
+if (typeid(s1) != typeid(size_t()) || typeid(s2) != typeid(size_t()) || typeid(s3) != typeid(size_t())){
+cout<<"invalid type"<<endl;
+assert(0);
+}
 
 Matrix m1(s1,s2,0),m2(s2,s3,0),m3(s1,s3,0);
 
